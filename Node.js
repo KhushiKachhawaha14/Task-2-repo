5de1f207f31fa4ace9350e8687ed9c85934df227
KhushiKachhawaha14 <khushikachwaha26@gmail.com>
2025-10-26 19:23:17 +0530

    Update and rename JS Server to Node.js

---
+++ Node.js
@@ -0,0 +1,12 @@
+{
+  "name": "jenkins-pipeline-demo-app",
+  "version": "1.0.0",
+  "description": "A minimal Node.js application for demonstrating Jenkins CI/CD.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
