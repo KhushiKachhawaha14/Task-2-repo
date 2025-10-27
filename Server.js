90df96239a13bedc00262236ad8e47f539384a96
KhushiKachhawaha14 <khushikachwaha26@gmail.com>
2025-10-27 12:04:55 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const app = express();
-const port = 8080; // Must match the port exposed in the Dockerfile
+const port = 8081; // Must match the port exposed in the Dockerfile
 
 app.get('/', (req, res) => {
   // Respond with a simple message showing the app is running
