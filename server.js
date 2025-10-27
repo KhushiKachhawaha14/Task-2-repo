b277ecf9af5230c5205ba098c57a2444d3ecb2db
KhushiKachhawaha14 <khushikachwaha26@gmail.com>
2025-10-27 12:19:59 +0530

    Change server port from 8080 to 8081

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   // Respond with a simple message showing the app is running
   res.send('<h1>Jenkins CI/CD Pipeline Success!</h1><p>Application is running inside the Docker container.</p>');
 });
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const HOST = '0.0.0.0'; // <--- MUST be 0.0.0.0 for Docker
 app.listen(port, () => {
   console.log(`Application successfully deployed and listening at http://localhost:${port}`);
